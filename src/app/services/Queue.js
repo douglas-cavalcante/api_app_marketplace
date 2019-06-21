4ab3ab464dd67e1c2119d588469db18d2edb37cb
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-06-21 15:15:57 -0300

    Adiciona monitoramento da fila de emails

--- src/app/services/Queue.js
+++ src/app/services/Queue.js
@@ -1,5 +1,6 @@
 const kue = require("kue");
 const redisConfig = require("../../config/redis");
+const Sentry = require("@sentry/node");
 
 const jobs = require("../jobs");
 
@@ -7,4 +8,5 @@ const Queue = kue.createQueue({ redis: redisConfig });
 
 Queue.process(jobs.PurchaseMail.key, jobs.PurchaseMail.handle);
 
+Queue.on("error", Sentry.captureException);
 module.exports = Queue;
